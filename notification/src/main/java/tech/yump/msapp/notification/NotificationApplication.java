8462aaa1d77ce74a04518d98ee22c445d3e14b01
Roberto Castro <rob1fcv21@gmail.com>
2022-08-13 19:11:19 -0600

    AMQP Notification

--- notification/src/main/java/tech/yump/msapp/notification/NotificationApplication.java
+++ notification/src/main/java/tech/yump/msapp/notification/NotificationApplication.java
@@ -1,12 +1,26 @@
 package tech.yump.msapp.notification;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import tech.yump.msaap.amqp.RabbitMQMessageProducer;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages = {"tech.yump.msapp.amqp"})
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(
+            RabbitMQMessageProducer producer,
+            NotificationConfig notificationConfig) {
+        return args -> {
+            producer.publish(
+                    "foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+        };
     }
 }
